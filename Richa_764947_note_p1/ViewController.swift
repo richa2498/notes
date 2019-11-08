d475483f69bf201678445474810795dab5a1e63e
richa <richa1998.patel@gmail.com>
2019-11-08 16:46:56 -0500

    version 5th - with the navigation editing [test part1]

--- Richa_764947_note_p1/ViewController.swift
+++ Richa_764947_note_p1/ViewController.swift
@@ -11,14 +11,8 @@ import UIKit
 @available(iOS 13.0, *)
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
   
-    
-
+  
     @IBOutlet weak var nav_name: UINavigationItem!
-    
-    @IBOutlet weak var folder: UILabel!
-    @IBOutlet weak var nav_bar: UINavigationBar!
-    //@IBOutlet weak var edit_btn: UINavigationItem!
-
     @IBOutlet weak var editBtn: UIBarButtonItem!
     var notes : [String] = []
     @IBOutlet weak var add_folder: UIBarButtonItem!
@@ -30,13 +24,12 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         notes = []
-       
-       
-        
+        //MARK: TO ENABLE SCROLLING ONE
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-
-    @IBOutlet weak var flg: UILabel!
+    
+    //MARK : TO ENABLE EDITING FOR THE REAARANGINFG FOLDERS
     @IBAction func on_edit(_ sender: UIBarButtonItem) {
         
         if editBtn.title == "Edit"
@@ -51,11 +44,9 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             tabl_view.isEditing = false
             
         }
-        
-        
-        
+     
     }
-    
+    //MARK : TO ADD FOLDERS DYNAMICALLY
     @IBAction func add_new_folder(_ sender: Any) {
      
         
@@ -63,6 +54,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         alert?.addTextField(configurationHandler: { (utxt) in
             utxt.placeholder = "Name"
         })
+        
         alert?.addAction(UIAlertAction(title: "Cancle?", style: .destructive, handler: nil))
         alert?.addAction(UIAlertAction(title: "Add New Folder", style: .default, handler: { (act) in
             let name = self.alert?.textFields?.first?.text
@@ -73,30 +65,28 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
          
             
         }))
+        
         present(alert!, animated: true, completion: nil)
     }
     
-    
+    //MARK : RETURNS THE ROWS OF TABLES
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return notes.count
       }
     
     
-      
+      //MARK : DEFINE CELL
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        
-        
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "folder")
-        
-            cell.imageView?.image = #imageLiteral(resourceName: "folder-icon.png")
-            cell.textLabel?.text = self.notes[indexPath.row]
-        
-       
+        var cell : UITableViewCell
+        cell = UITableViewCell(style: .default, reuseIdentifier: "folder")
+        cell.imageView?.image = #imageLiteral(resourceName: "folder-icon.png")
+        cell.textLabel?.text = self.notes[indexPath.row]
+        return cell
       
-          return cell
-      }
+    }
+    
+    //MARK : FOR REAARANGING THE FOLDERS
    
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
@@ -114,18 +104,16 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         notes[sourceIndexPath.row] = notes[temp]
     }
     
+    //MARK : TO DISABLE DELETE  FUNCTIONALITY WHILE EDITING
     func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
         return false
     }
-    
- 
-   
-    
+
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .none
     }
     
-    
+    //MARK : DELETE DATA
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let delete = UIContextualAction(style: .destructive, title: "delete") { (act, v, _) in
             self.notes.remove(at: indexPath.row)
@@ -136,21 +124,6 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
         
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        
-       // nav_name.barTintColor = UIColor(red: 0.01560127642, green: 0.01560719963, blue: 0.01560719963, alpha: 0.5)
-        nav_name.title = "Folder"
-        flg.isHidden = true
-       
-     
-    }
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-      //  nav_bar.barTintColor =  #colorLiteral(red: 1, green: 0, blue: 0.1814254509, alpha: 0.8470588235)
-        nav_name.title = ""
-          flg.isHidden = false
-     
-    }
-
     
     
 }
